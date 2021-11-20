ec68cc024cf262e0bbcd1dda527edf1c2935488d
phuong truc <1854050125truc@ou.edu.vn>
2021-11-20 13:19:18 +0700

    Them dieu kien button Cong

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -20,10 +20,13 @@ namespace Calculator
         private void btCong_Click(object sender, EventArgs e)
         {
             int a, b, kQ;
-            a = int.Parse(txtSoThu1.Text);
-            b = int.Parse(txtSoThu2.Text);
-            kQ = a + b;
-            txtKQ.Text = kQ.ToString();
+            if (txtSoThu1.Text != String.Empty && txtSoThu2.Text != String.Empty)
+            {
+                a = int.Parse(txtSoThu1.Text);
+                b = int.Parse(txtSoThu2.Text);
+                kQ = a + b;
+                txtKQ.Text = kQ.ToString(); 
+            }
         }
 
         private void btTru_Click(object sender, EventArgs e)
